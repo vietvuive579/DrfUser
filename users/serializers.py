5591e2ebfaa18a6ceec5c96a45bff4c966a878c9
vietvuive579 <vietvuive579@gmail.com>
2022-05-16 18:20:07 +0700

    first commit

---
+++ users/serializers.py
@@ -0,0 +1,53 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers, validators
+
+
+class BaseInfoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ("username", "password", "email", "first_name", "last_name")
+        extra_kwargs = {
+            "first_name": {"required": True},
+            "last_name": {"required": True},
+            "password": {"write_only": True},
+            "username": {
+                "required": True,
+                "allow_blank": False,
+                "validators": [
+                    validators.UniqueValidator(
+                        User.objects.all(), f"Username already exists."
+                    )
+                ],
+            },
+            "email": {
+                "required": True,
+                "allow_blank": False,
+                "validators": [
+                    validators.UniqueValidator(
+                        User.objects.all(), f"A user with that Email already exists."
+                    )
+                ],
+            },
+        }
+
+class RegisterSerializer(BaseInfoSerializer):
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data["username"],
+            email=validated_data["email"],
+            password=validated_data["password"],
+            first_name=validated_data["first_name"],
+            last_name=validated_data["last_name"]
+        )
+        return user
+
+class UpdateSerializer(BaseInfoSerializer):
+    def create(self, validated_data):
+        user = User.objects.get(
+            username=validated_data["username"],
+            email=validated_data["email"],
+            password=validated_data["password"],
+            first_name=validated_data["first_name"],
+            last_name=validated_data["last_name"]
+        )
+        return user
